cde0a12a7c59de98ba92a608c083e565d7d6fb38
qahwa <qahwa@LAPTOP-DHV75JN1>
2021-03-23 14:58:11 +0300

    class object for Employees

---
+++ Employees/src/Ahmed.java
@@ -0,0 +1,17 @@
+
+public class Ahmed {
+
+	public static void main(String[] args) {
+		
+		Employees Ahmed =new Employees("Ahmed",5000,"second","programming",26);
+		
+		System.out.println(Ahmed.name);
+		System.out.println(Ahmed.salary);
+		System.out.println(Ahmed.rank);
+		System.out.println(Ahmed.section);
+		System.out.println(Ahmed.age);
+		
+
+	}
+
+}
---
+++ Employees/src/Employees.java
@@ -0,0 +1,21 @@
+
+public class Employees {
+
+	String name;
+	int salary;
+	String rank;
+	String section;
+	int age;
+	
+	Employees(){
+}
+
+	
+	Employees(String newname, int newsalary,String newrank,String newsection,int newage){
+		name=newname;
+		salary=newsalary;
+		rank=newrank;
+		section=newsection;
+		age=newage;
+	}
+	}
